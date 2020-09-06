549b3906f46c3cbe598ac1284ef3acb21d37152a
nilodude <alphone@protonmail.com>
2020-09-06 17:18:56 +0200

    trying to do things as a normal person

--- sketch.js
+++ sketch.js
@@ -1,71 +1,47 @@
-var cols, rows;
-var scl = 20;
-var w = 1400;
-var h = 1000;
-
-var flying = 0;
-var rotY = 14;
-var terrain = [];
-let spectrum = null;
+var hist = [];
 
 function preload() {
 	sound = loadSound('scorpio-demo02.mp3');
 }
 
 function setup() {
-	let cnv = createCanvas(600, 600, WEBGL);
-	cols = 64;
-	rows = 64;
-
-	for (var x = 0; x < cols; x++) {
-		terrain[x] = [];
-		for (var y = 0; y < rows; y++) {
-			terrain[x][y] = 0; //specify a default value for now
-		}
-	}
-
+	let cnv = createCanvas(700, 500, WEBGL);
 	cnv.mouseClicked(togglePlay);
-	fft = new p5.FFT(0.3, cols);
+	fft = new p5.FFT(0.8, 512);
+	fft.smooth();
 	sound.amp(0.2);
-
 }
 
 function draw() {
-	spectrum = fft.analyze();
 
-	console.log(spectrum);
-	flying += 0.1;
-	var yoff = flying;
-	for (var y = 0; y < rows; y++) {
-		var xoff = 0;
-		for (var x = 0; x < cols; x++) {
-			terrain[x][y] = map(noise(xoff, yoff), 0, 1, -100, 100);
-			xoff += 0.2;
-		}
-		yoff += 0.2;
-	}
-
+	camera(mouseX - width / 2, mouseY - height / 2, (height / 2.0) / tan(PI * 30.0 / 180.0), 0, 0, 0, 0, 1, 0);
 	background(0);
-	translate(0, 50);
-	rotateX(PI / 3);
+	translate(-width / 2, -height / 2, -500)
 
-	var R = map(sin(47 * frameCount / 10000), 0, 1, 100, 200);
-	var G = map(0.5 * sin(9 * frameCount / 10000), 0, 1, 100, 200);
-	var B = map(0.8 * sin(55 * frameCount / 10000), 0, 1, 100, 200);
-
-	fill(R, G, B, 50);
-	translate(-w / 2, -h / 2);
-	for (var y = 0; y < rows - 1; y++) {
-		beginShape(TRIANGLE_STRIP);
-		for (var x = 0; x < cols; x++) {
-			// rotateY(PI * rotY++); // añade lineas to raras
-			vertex(x * scl, y * scl, terrain[x][y]);
-			vertex(x * scl, (y + 1) * scl, terrain[x][y + 1]);
-
-		}
-		endShape();
+	let spectrum = fft.analyze();
+	hist.push(spectrum);
+	noStroke();
+	fill(255, 0, 255);
+	for (let i = 0; i < spectrum.length; i++) {
+		let x = map(i, 0, spectrum.length, 0, width);
+		let h = -height + map(spectrum[i], 0, 255, height, 0);
+		rect(x, height, width / spectrum.length, h)
 	}
+
+	let waveform = fft.waveform();
+	// noFill();
+	beginShape();
+	stroke(20);
+	for (let i = 0; i < waveform.length; i++) {
+		let x = map(i, 0, waveform.length, 0, width);
+		let y = map(waveform[i], -1, 1, 0, height);
+		vertex(x, y);
+	}
+	endShape();
+
+	// text('tus muertos', 20, 20);
 }
+
 function togglePlay() {
 	if (sound.isPlaying()) {
 		sound.pause();
