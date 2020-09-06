11b9d3362cbf433493c8e8f34ccb7b153aa81e16
nilodude <alphone@protonmail.com>
2020-09-06 16:27:33 +0200

    basura

--- sketch.js
+++ sketch.js
@@ -1,21 +1,23 @@
 var cols, rows;
-var scl = 20;
+var scl = 1;
 var w = 1400;
 var h = 1000;
 
 var flying = 0;
 var rotY = 14;
 var terrain = [];
-let spectrum = null;
+var spectrum = [];
 
 function preload() {
 	sound = loadSound('scorpio-demo02.mp3');
 }
 
 function setup() {
-	let cnv = createCanvas(600, 600, WEBGL);
-	cols = 64;
-	rows = 64;
+	let cnv = createCanvas(600, 500, WEBGL);
+	camera();
+
+	cols = 200;
+	rows = cols;
 
 	for (var x = 0; x < cols; x++) {
 		terrain[x] = [];
@@ -25,28 +27,31 @@ function setup() {
 	}
 
 	cnv.mouseClicked(togglePlay);
-	fft = new p5.FFT(0.3, cols);
-	sound.amp(0.2);
-
+	fft = new p5.FFT(0.3, 64);
+	sound.amp(0.4);
+	fft.smooth(0.4)
 }
 
 function draw() {
-	spectrum = fft.analyze();
+	orbitControl();
 
-	console.log(spectrum);
-	flying += 0.1;
+
+	flying++;
 	var yoff = flying;
-	for (var y = 0; y < rows; y++) {
-		var xoff = 0;
-		for (var x = 0; x < cols; x++) {
-			terrain[x][y] = map(noise(xoff, yoff), 0, 1, -100, 100);
-			xoff += 0.2;
-		}
-		yoff += 0.2;
+	// for (var y = 0; y < rows; y++) {
+	spectrum.push([]);
+	spectrum.push(fft.analyze());
+	console.log(spectrum, frameCount);
+	var xoff = 0;
+	for (var x = 0; x < 50; x++) {
+		// terrain[x][y] = map(noise(xoff, yoff), 0, 1, -100, 100);
+		terrain[x][frameCount] = map(spectrum[x], 10, 255, -50, -10)
+		xoff += 0.2;
 	}
+	yoff += 0.2;
+	// }
 
 	background(0);
-	translate(0, 50);
 	rotateX(PI / 3);
 
 	var R = map(sin(47 * frameCount / 10000), 0, 1, 100, 200);
@@ -54,16 +59,20 @@ function draw() {
 	var B = map(0.8 * sin(55 * frameCount / 10000), 0, 1, 100, 200);
 
 	fill(R, G, B, 50);
-	translate(-w / 2, -h / 2);
+	translate(0, 0, 0);
 	for (var y = 0; y < rows - 1; y++) {
 		beginShape(TRIANGLE_STRIP);
-		for (var x = 0; x < cols; x++) {
+		let decre = Math.log(y ^ (1 / 2) + 1);
+		for (var x = 0; x < 64; x++) {
 			// rotateY(PI * rotY++); // añade lineas to raras
 			vertex(x * scl, y * scl, terrain[x][y]);
 			vertex(x * scl, (y + 1) * scl, terrain[x][y + 1]);
-
 		}
 		endShape();
+	}
+
+	if (spectrum.length > rows - 5) {
+		spectrum.splice(0, 1);
 	}
 }
 function togglePlay() {
